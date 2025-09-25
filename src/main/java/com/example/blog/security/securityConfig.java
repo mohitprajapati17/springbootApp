bfb5317d654b36ef530d7577e195bfcc7ad7a6a7
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-09-26 02:47:41 +0530

    security

---
+++ src/main/java/com/example/blog/security/securityConfig.java
@@ -0,0 +1,69 @@
+package com.example.blog.security;
+
+
+import java.net.http.HttpClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.blog.service.MyUserDetailsService;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class securityConfig {
+
+    @Autowired
+    public Jwtfilter jwtfilter;
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12)
+        );
+        return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(customizer->customizer.disable())
+        .authorizeHttpRequests(request->request
+         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+         .requestMatchers("login").permitAll()
+         .requestMatchers("register").permitAll()
+         .requestMatchers("/**").authenticated()
+        );
+
+        http.authenticationProvider(authProvider());
+        http.addFilterBefore(jwtfilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager  (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+
+    
+}
---
+++ src/main/java/com/example/blog/service/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.blog.models.Users;
+import com.example.blog.repo.UserRepository;
+
+import java.util.ArrayList;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { //Doubt about why implement this when you can make your own
+        Users user =userRepository.findByUsername(username);
+        if(user==null){
+
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return  new UserPrinciple(user);
+    }
+
+
+    
+}
