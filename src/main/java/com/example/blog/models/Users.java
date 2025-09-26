eb4d7a5885d43899e310ba2bd678781a5de3651d
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-09-27 03:08:21 +0530

    comments all

--- src/main/java/com/example/blog/models/Users.java
+++ src/main/java/com/example/blog/models/Users.java
@@ -6,16 +6,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Users entity representing a blog user
+ * Maps to the 'users' table in the database
+ * Uses Lombok annotations for automatic getter/setter generation
+ */
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data                    // Generates getters, setters, toString, equals, and hashCode
+@NoArgsConstructor       // Generates no-argument constructor
+@AllArgsConstructor      // Generates constructor with all arguments
 public class Users {
+    
+    // Primary key - username is used as the unique identifier
     @Id
+    public String username;
+    
+    // User's password (will be encrypted before storing)
+    public String password;
 
-    public  String username ;
-    public  String password;
-
+    // User's email address
     public String email;
-    public String roles="USER";
+    
+    // User's role (defaults to "USER")
+    public String roles = "USER";
 }
--- src/main/java/com/example/blog/service/UserPrinciple.java
+++ src/main/java/com/example/blog/service/UserPrinciple.java
@@ -9,45 +9,84 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.blog.models.Users;
 
+/**
+ * Custom UserDetails implementation for Spring Security
+ * Wraps the Users entity to make it compatible with Spring Security authentication
+ */
 public class UserPrinciple implements UserDetails {
 
+    // Reference to the Users entity
     private Users user;
+    
+    /**
+     * Constructor that takes a Users entity
+     * @param user - The Users entity to wrap
+     */
     public UserPrinciple(Users user){
-        this.user=user;
+        this.user = user;
     }
+    
+    /**
+     * Get user authorities/roles
+     * @return Collection of granted authorities (roles)
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Return a single "USER" authority for all users
         return Collections.singleton(new SimpleGrantedAuthority("USER"));
     }   
 
+    /**
+     * Get user's password
+     * @return User's encrypted password
+     */
     @Override
     public String getPassword() {
         return user.getPassword();
     }
 
+    /**
+     * Get user's username
+     * @return User's username
+     */
     @Override
     public String getUsername() {
         return user.getUsername();
     }
 
+    /**
+     * Check if account is not expired
+     * @return true (account never expires in this implementation)
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /**
+     * Check if account is not locked
+     * @return true (account is never locked in this implementation)
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /**
+     * Check if credentials are not expired
+     * @return true (credentials never expire in this implementation)
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /**
+     * Check if account is enabled
+     * @return true (account is always enabled in this implementation)
+     */
     @Override
     public boolean isEnabled() {
         return true;
     }
-    
 }
