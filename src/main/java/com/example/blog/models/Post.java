f1c7a0ae821871c0359eada8ccc7f1288101e2b4
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-09-27 03:08:08 +0530

    comments models

--- src/main/java/com/example/blog/models/Post.java
+++ src/main/java/com/example/blog/models/Post.java
@@ -1,30 +1,36 @@
 package com.example.blog.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * Post entity representing a blog post
+ * Maps to the 'post' table in the database
+ */
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Post {
 
+    // Primary key with auto-generated ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
+    
+    // Title of the blog post
     public String title;
+    
+    // Content of the blog post (stored as TEXT to allow long content)
     @Column(columnDefinition = "TEXT")
     public String content;
 
+    // Many-to-One relationship: Many posts can belong to one user
+    // This creates a foreign key relationship to the Users table
     @ManyToOne
     public Users author;
+    
+    // Timestamp when the post was created
     public LocalDateTime createdAt;
-    public LocalDateTime  updatedAt;
-
-
-
+    
+    // Timestamp when the post was last updated
+    public LocalDateTime updatedAt;
 }
